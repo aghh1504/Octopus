adcac644310ced49a82662e15d82504fd838645e
aghh1504 <agatta1504@interia.pl>
2019-10-27 15:38:25 +0000

    Updates

---
+++ precache-manifest.78747ac6e08240765f5758785daa30e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae796bdf18150b808672079548f9b80",
+    "url": "/Octopus/index.html"
+  },
+  {
+    "revision": "16386abeb8eb1964704d",
+    "url": "/Octopus/static/js/2.81fa84b0.chunk.js"
+  },
+  {
+    "revision": "c6641d77d90d9e77bb00",
+    "url": "/Octopus/static/js/main.c6253bc0.chunk.js"
+  },
+  {
+    "revision": "b60642638ae8d5be22aa",
+    "url": "/Octopus/static/js/runtime-main.4246c54d.js"
+  },
+  {
+    "revision": "385882e8b3f4d5d4a85bf2eb2cd420fb",
+    "url": "/Octopus/static/media/bulb.385882e8.jpg"
+  },
+  {
+    "revision": "224f08b4f36b5893c91a76780171913f",
+    "url": "/Octopus/static/media/octopus_logo.224f08b4.png"
+  }
+]);
