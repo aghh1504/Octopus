2baf0d7640c61ec291c882f6a364bbac68e08a3d
aghh1504 <agatta1504@interia.pl>
2019-10-29 08:56:00 +0000

    Add test for order reducer

--- src/reducers/tests/order.test.jsx
+++ src/reducers/tests/order.test.jsx
@@ -7,9 +7,26 @@ const setOrderAction = {
   }
 };
 
+const decrementQuantityAction = {
+  type: "DECREMENT_QUANTITY"
+};
+const incrementQuantityAction = {
+  type: "INCREMENT_QUANTITY"
+};
+
 describe("Order Reducer", () => {
   it("should return correctly", () => {
     const result = order(undefined, setOrderAction);
     expect(result).toEqual({ quantity: 1, status: "OK" });
+  });
+
+  it("should return correct value for quantity when decrementQuantity", () => {
+    const result = order(undefined, decrementQuantityAction);
+    expect(result).toEqual({ quantity: 0, status: undefined });
+  });
+
+  it("should return correct value for quantity when incrementQuantity", () => {
+    const result = order(undefined, incrementQuantityAction);
+    expect(result).toEqual({ quantity: 2, status: undefined });
   });
 });
